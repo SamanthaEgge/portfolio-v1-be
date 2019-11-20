d205c034f1beeac1df62378719fc16401c0f2c1d
Samantha Egge <samantha.c.egge@gmail.com>
2019-11-20 13:17:14 -0800

    Slight modifications in many-to-many tables.

--- __tests__/user.js
+++
@@ -1,6 +0,0 @@
-const request = require('supertest');
-
-const db = require('../data/dbConfig.js');
-const server = require('./server.js');
-
-describe('user', )
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
+const pg = require('pg');
+pg.defaults.ssl = true;
+
 module.exports = {
+  
+  /// Development if you host locally
   development: {
-    client: 'postgres',
+    client: 'pg',
     connection: {
       host: 'localhost',
-      database: 'portfoliov1',
-      user:     "postgres",
-      password: "AustinCityLimits"
+      database: 'portfoliov1', // What the DB should be named locally
+      user:     "postgres", // This should be the default unless you have changed it
+      password: process.env.POSTGRES_PASS // The password you set for POSTGRES not PgAdmin
     },
     migrations: {
       directory: './data/migrations'
@@ -14,19 +20,39 @@ module.exports = {
       directory: './data/seeds'
     },
   },
-  testing: {
-    client: 'postgres',
-    connection: {
-      host: 'localhost',
-      database: 'portfoliov1test',
-      user:     "postgres",
-      password: "AustinCityLimits"
+
+  //// Standard development/production set up if you're using Heroku only
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
+      tableName: 'knex_migrations',
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
     },
-  } 
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    useNullAsDefault: true
+  }
 };
