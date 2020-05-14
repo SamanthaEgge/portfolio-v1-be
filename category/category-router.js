871535df6b469f43e610349480fbd16cda2ba407
Samantha Egge <samantha.c.egge@gmail.com>
2020-05-14 11:01:07 -0700

    Fixed one more path error.

--- category/category-router.js
+++ category/category-router.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 const Categories = require('./category-model.js')
-const restricted = require('../middleware/restricted.js')
+// const restricted = require('../middleware/restricted.js')
 
 //// Public routes
 router.get('/', (request, response) => {
